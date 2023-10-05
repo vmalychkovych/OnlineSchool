ef2caac31516e6e6a2b03f1c25ea0764d82884fd
vmalychkovych <viktor.malychkovych.knm.2020@gmail.com>
2023-10-05 16:02:58 +0300

    Connect to databaces

---
+++ DB/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineSchool.DB
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
